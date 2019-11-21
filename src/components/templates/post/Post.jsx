64cfe42adbb005ae47f9f69fdd40fb620e3e2479
Jack Pritchard <jack@noface.co.uk>
2019-11-21 16:57:47 +0000

    Add post date to blog posts

--- src/components/templates/post/Post.jsx
+++ src/components/templates/post/Post.jsx
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import ParseHTML from "../../particles/ParseHTML"
 import { decodeHTML } from "../../helpers"
 import { InView } from "react-intersection-observer"
+import moment from "moment"
 
 import { Article, ArticleIntro } from "./PostStyles"
 import { RelatedContainer, RelatedItem } from "./RelatedStyles"
@@ -12,63 +13,66 @@ import Base from "../Base"
 import ImageLoader from "../../molecules/imageloader/ImageLoader"
 
 const PostTemplate = ({ pageContext }) => {
-  const { content, PostFields, title } = pageContext
+	const { content, date, PostFields, title } = pageContext
 
-  const related =
-    PostFields &&
-    PostFields.relatedPosts &&
-    PostFields.relatedPosts.length &&
-    PostFields.relatedPosts
+	const related =
+		PostFields &&
+		PostFields.relatedPosts &&
+		PostFields.relatedPosts.length &&
+		PostFields.relatedPosts
 
-  return (
-    <Base context={pageContext}>
-      <ArticleIntro>
-        <Link to="/posts">Insights</Link>
-        <h1>{decodeHTML(title)}</h1>
-      </ArticleIntro>
-      <Article>{ParseHTML(content)}</Article>
-      {related && related.length > 0 && <Related data={related} />}
-    </Base>
-  )
+	const postDate = new Date(date)
+
+	return (
+		<Base context={pageContext}>
+			<ArticleIntro>
+				<Link to="/posts">Insights</Link>
+				<h1>{decodeHTML(title)}</h1>
+				<h4>{moment(postDate).format("DD/MM/YYYY")} by Jack Pritchard</h4>
+			</ArticleIntro>
+			<Article>{ParseHTML(content)}</Article>
+			{related && related.length > 0 && <Related data={related} />}
+		</Base>
+	)
 }
 
 function Related({ data }) {
-  return (
-    <RelatedContainer>
-      <h2>
-        Continue Reading{" "}
-        <span aria-label="book pile emoji" role="img">
-          📚
-        </span>
-      </h2>
-      <div className="related__items">
-        {data.map(
-          item =>
-            item.featuredImage && (
-              <InView key={item.uri} threshold={0} triggerOnce={true}>
-                {({ inView, ref }) => (
-                  <RelatedItem ref={ref}>
-                    <Link to={`/${item.uri}`}>
-                      <ImageLoader
-                        src={item.featuredImage.md}
-                        alt={
-                          item.featuredImage.altText
-                            ? item.featuredImage.altText
-                            : item.title
-                        }
-                        className="related__media"
-                      />
-                      {item.title && <h3>{decodeHTML(item.title)}</h3>}
-                      {item.seo.metaDesc && <p>{item.seo.metaDesc}</p>}
-                    </Link>
-                  </RelatedItem>
-                )}
-              </InView>
-            )
-        )}
-      </div>
-    </RelatedContainer>
-  )
+	return (
+		<RelatedContainer>
+			<h2>
+				Continue Reading{" "}
+				<span aria-label="book pile emoji" role="img">
+					📚
+				</span>
+			</h2>
+			<div className="related__items">
+				{data.map(
+					item =>
+						item.featuredImage && (
+							<InView key={item.uri} threshold={0} triggerOnce={true}>
+								{({ inView, ref }) => (
+									<RelatedItem ref={ref}>
+										<Link to={`/${item.uri}`}>
+											<ImageLoader
+												src={item.featuredImage.md}
+												alt={
+													item.featuredImage.altText
+														? item.featuredImage.altText
+														: item.title
+												}
+												className="related__media"
+											/>
+											{item.title && <h3>{decodeHTML(item.title)}</h3>}
+											{item.seo.metaDesc && <p>{item.seo.metaDesc}</p>}
+										</Link>
+									</RelatedItem>
+								)}
+							</InView>
+						)
+				)}
+			</div>
+		</RelatedContainer>
+	)
 }
 
 export default PostTemplate
