7d8e3f62fb4ab55495a76d661f785a30e805aa82
Jack Pritchard <jack@noface.co.uk>
2019-11-23 13:46:00 +0000

    Fix ref to node in Grid Component

--- plugins/gatsby-source-bonus/gatsby-node.js
+++ plugins/gatsby-source-bonus/gatsby-node.js
@@ -1,63 +1,61 @@
 require("dotenv").config({
-	path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.${process.env.NODE_ENV}`,
 })
 
 const fetch = require(`node-fetch`)
-const queryString = require(`query-string`)
 const crypto = require(`crypto`)
-const path = require(`path`)
 
 exports.sourceNodes = async (
-	{ actions: { createNode }, createNodeId },
-	{ plugins, ...options }
+  { actions: { createNode }, createNodeId },
+  { plugins, ...options }
 ) => {
-	const accessToken = process.env.GATSBY_DRIBBBLE_TOKEN
-	const dribbbleURL = `https://api.dribbble.com/v2/user/shots?access_token=${accessToken}`
-	const dribbbleResponse = await fetch(dribbbleURL)
-	const dribbbleData = await dribbbleResponse.json()
+  const accessToken = process.env.GATSBY_DRIBBBLE_TOKEN
+  const dribbbleURL = `https://api.dribbble.com/v2/user/shots?access_token=${accessToken}`
+  const dribbbleResponse = await fetch(dribbbleURL)
+  const dribbbleData = await dribbbleResponse.json()
 
-	if (dribbbleData && dribbbleData.length > 0) {
-		dribbbleData.forEach(shot => {
-			createNode({
-				...shot,
-				id: createNodeId(`dribbble-${shot.id}`),
-				parent: null,
-				children: [],
-				internal: {
-					type: "Dribbble",
-					content: JSON.stringify(shot),
-					contentDigest: crypto
-						.createHash("md5")
-						.update(JSON.stringify(shot))
-						.digest("hex"),
-				},
-			})
-		})
-	}
+  if (dribbbleData && dribbbleData.length > 0) {
+    dribbbleData.forEach(shot => {
+      createNode({
+        ...shot,
+        id: createNodeId(`dribbble-${shot.id}`),
+        parent: null,
+        children: [],
+        internal: {
+          type: "Dribbble",
+          content: JSON.stringify(shot),
+          contentDigest: crypto
+            .createHash("md5")
+            .update(JSON.stringify(shot))
+            .digest("hex"),
+        },
+      })
+    })
+  }
 
-	const GAPI = process.env.GATSBY_GOOGLE_API
-	const PlayListID = `UUIOm-HME4V_STS9yWM5aXIg`
-	const NumberResults = 12
-	const youtubeURL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=${PlayListID}&key=${GAPI}&maxResults=${NumberResults}`
-	const youtubeResponse = await fetch(youtubeURL)
-	const youtubeData = await youtubeResponse.json()
+  const GAPI = process.env.GATSBY_GOOGLE_API
+  const PlayListID = `UUIOm-HME4V_STS9yWM5aXIg`
+  const NumberResults = 12
+  const youtubeURL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=${PlayListID}&key=${GAPI}&maxResults=${NumberResults}`
+  const youtubeResponse = await fetch(youtubeURL)
+  const youtubeData = await youtubeResponse.json()
 
-	if (youtubeData && youtubeData.items) {
-		youtubeData.items.forEach(video => {
-			createNode({
-				...video,
-				id: createNodeId(`youtube-${video.id}`),
-				parent: null,
-				children: [],
-				internal: {
-					type: "Youtube",
-					content: JSON.stringify(video),
-					contentDigest: crypto
-						.createHash("md5")
-						.update(JSON.stringify(video))
-						.digest("hex"),
-				},
-			})
-		})
-	}
+  if (youtubeData && youtubeData.items) {
+    youtubeData.items.forEach(video => {
+      createNode({
+        ...video,
+        id: createNodeId(`youtube-${video.id}`),
+        parent: null,
+        children: [],
+        internal: {
+          type: "Youtube",
+          content: JSON.stringify(video),
+          contentDigest: crypto
+            .createHash("md5")
+            .update(JSON.stringify(video))
+            .digest("hex"),
+        },
+      })
+    })
+  }
 }
--- src/components/organisms/grid/Grid.jsx
+++ src/components/organisms/grid/Grid.jsx
@@ -1,78 +1,78 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
-import GridComponent from "./GridStyles";
+import GridComponent from "./GridStyles"
 
 function Grid({ filter, items }) {
-	const [count, updateCount] = useState(100);
+  const [count, updateCount] = useState(100)
 
-	const addItems = () => {
-		updateCount(count + 9);
-	};
+  const addItems = () => {
+    updateCount(count + 9)
+  }
 
-	var rows = [];
-	for (var i = 0; i < count; i++) {
-		const item = items[i];
+  var rows = []
+  for (var i = 0; i < count; i++) {
+    const item = items[i]
 
-		if (typeof item === "undefined") break;
+    if (typeof item === "undefined") break
 
-		const media = item.node.media;
-		const ext = media.substr(media.lastIndexOf(".") + 1);
+    const media = item.media
+    const ext = media.substr(media.lastIndexOf(".") + 1)
 
-		if (ext === `mp4`) {
-			rows.push(
-				<GridItem
-					key={`grid-item-${i}`}
-					category={item.node.category}
-					filter={filter}
-				>
-					<video src={item.node.media} alt="" autoPlay muted loop />
-					<span className="grid__item__title">{item.node.title}</span>
-				</GridItem>
-			);
-		} else {
-			rows.push(
-				<GridItem
-					key={`grid-item-${i}`}
-					category={item.node.category}
-					filter={filter}
-				>
-					<img src={item.node.media} alt="" />
-					<span className="grid__item__title">{item.node.title}</span>
-				</GridItem>
-			);
-		}
-	}
+    if (ext === `mp4`) {
+      rows.push(
+        <GridItem
+          key={`grid-item-${i}`}
+          category={item.category}
+          filter={filter}
+        >
+          <video src={item.media} alt="" autoPlay muted loop />
+          <span className="grid__item__title">{item.title}</span>
+        </GridItem>
+      )
+    } else {
+      rows.push(
+        <GridItem
+          key={`grid-item-${i}`}
+          category={item.category}
+          filter={filter}
+        >
+          <img src={item.media} alt="" />
+          <span className="grid__item__title">{item.title}</span>
+        </GridItem>
+      )
+    }
+  }
 
-	return (
-		<>
-			<GridComponent>{rows}</GridComponent>
-			<button disabled={count > items.length} onClick={addItems}>
-				Load More
-			</button>
-		</>
-	);
+  return (
+    <>
+      <GridComponent>{rows}</GridComponent>
+      <button disabled={count > items.length} onClick={addItems}>
+        Load More
+      </button>
+    </>
+  )
 }
 
 function GridItem({ category, children, filter }) {
-	const [fullScreen, changeFullScreen] = useState(false);
+  const [fullScreen, changeFullScreen] = useState(false)
 
-	const toggleFullscreen = e => {
-		changeFullScreen(!fullScreen);
-	};
+  const toggleFullscreen = e => {
+    changeFullScreen(!fullScreen)
+  }
 
-	let classList = `grid__item`;
-	if (fullScreen) classList += ` grid__item--fullscreen`;
+  let classList = `grid__item`
+  if (fullScreen) classList += ` grid__item--fullscreen`
 
-	if (filter !== ``) {
-		if (category === filter) classList += ` grid__item--active`;
-		if (category !== filter) classList += ` grid__item--inactive`;
-	}
+  if (filter !== ``) {
+    if (category === filter) classList += ` grid__item--active`
+    if (category !== filter) classList += ` grid__item--inactive`
+  }
 
-	return (
-		<div className={classList} onClick={toggleFullscreen}>
-			{children}
-		</div>
-	);
+  return (
+    <div className={classList} onClick={toggleFullscreen}>
+      {children}
+    </div>
+  )
 }
 
-export default Grid;
+export default Grid
