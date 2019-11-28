c028cb7b1a600397c916564cd823e81417cb7c86
Jack Pritchard <jack@noface.co.uk>
2019-11-28 03:58:33 +0000

    TSX - Paragraph

--- src/components/atoms/Paragraph.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Paragraph({ children }) {
-	return <p>{children}</p>;
-}
-
-export default Paragraph;
---
+++ src/components/atoms/paragraph/Paragraph.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+type ParagraphProps = {
+  children: any
+}
+
+const Paragraph = ({ children }: ParagraphProps) => <p>{children}</p>
+
+export default Paragraph
