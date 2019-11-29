6612d175a630f3a80882d6e0350740042a7abb8e
Jack Pritchard <jack@noface.co.uk>
2019-11-29 07:52:25 +0000

    TSX - Header

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -10,7 +10,9 @@ import Logo from "../../../assets/images/logo/logo.svg"
 import IconBars from "../../../assets/images/icons/solid/bars.svg"
 import IconTimes from "../../../assets/images/icons/solid/times.svg"
 
-function Header() {
+type HeaderProps = {}
+
+const Header = ({}: HeaderProps) => {
   const [menuOpen, toggleMenu] = useState(false)
 
   const menuData = useSiteMenuData()
@@ -31,7 +33,7 @@ function Header() {
         <Link to="/" className="header__logo">
           <Logo />
         </Link>
-        <nav className={menuOpen ? `header__menu--show` : null}>
+        <nav className={menuOpen ? `header__menu--show` : undefined}>
           {headerData.menuItems.nodes.map(
             (item, index) =>
               index !== menuLength - 1 && (
@@ -52,7 +54,12 @@ function Header() {
   )
 }
 
-const MenuItem = ({ url, label }) => {
+type MenuItemProps = {
+  label: string
+  url: string
+}
+
+const MenuItem = ({ label, url }: MenuItemProps) => {
   const relativeLink = useRelative(url)
   return <Link to={relativeLink}>{label}</Link>
 }
