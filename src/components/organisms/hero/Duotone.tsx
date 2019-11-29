cc643c071b0a4ad8dfbeff2547249e26457cb767
Jack Pritchard <jack@noface.co.uk>
2019-11-29 07:52:30 +0000

    TSX - Duotone

--- src/components/organisms/hero/Duotone.tsx
+++ src/components/organisms/hero/Duotone.tsx
@@ -1,48 +1,57 @@
-import React, { Component } from "react";
-import styled from "styled-components";
+import React, { Component } from "react"
+import styled from "styled-components"
 
 const DuotoneElement = styled.picture`
-	--highlight: ${props => (props.highlight ? props.highlight : `#ff3ebf`)};
-	--shadow: ${props => (props.shadow ? props.shadow : `#4000ff`)};
+  --highlight: ${props =>
+    props.highlight ? props.highlight : props.theme.primary};
+  --shadow: ${props => (props.shadow ? props.shadow : `#000`)};
 
-	width: 100%;
-	height: 100%;
-	position: relative;
-	z-index: 2;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 2;
 
-	&::before,
-	&::after {
-		content: "";
-		display: block;
-		height: 100%;
-		left: 0;
-		position: absolute;
-		top: 0;
-		width: 100%;
-		z-index: 1;
+  &::before,
+  &::after {
+    content: "";
+    display: block;
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    z-index: 1;
 
-		opacity: 1;
-	}
+    opacity: 1;
+  }
 
-	&::before {
-		background-color: var(--highlight);
-		mix-blend-mode: darken;
-	}
+  &::before {
+    background-color: var(--highlight);
+    mix-blend-mode: darken;
+  }
 
-	&::after {
-		background-color: var(--shadow);
-		mix-blend-mode: lighten;
-	}
-`;
+  &::after {
+    background-color: var(--shadow);
+    mix-blend-mode: lighten;
+  }
+`
 
-export default class Duotone extends Component {
-	render() {
-		const { highlight, shadow } = this.props;
-
-		return (
-			<DuotoneElement className="duotone" highlight={highlight} shadow={shadow}>
-				{this.props.children}
-			</DuotoneElement>
-		);
-	}
+type DuotoneProps = {
+  children: any
+  className?: string
+  highlight?: string
+  shadow?: string
 }
+
+const Duotone = ({
+  children,
+  className = "duotone",
+  highlight,
+  shadow,
+}: DuotoneProps) => (
+  <DuotoneElement className={className} highlight={highlight} shadow={shadow}>
+    {children}
+  </DuotoneElement>
+)
+
+export default Duotone
