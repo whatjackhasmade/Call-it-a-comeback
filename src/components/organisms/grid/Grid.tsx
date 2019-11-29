2e776f400531cbe4b5dd58ec8957bd8667470fe9
Jack Pritchard <jack@noface.co.uk>
2019-11-29 07:52:20 +0000

    TSX - GridTSX - Grid

--- src/components/organisms/grid/Grid.tsx
+++ src/components/organisms/grid/Grid.tsx
@@ -2,59 +2,85 @@ import React, { useState } from "react"
 
 import GridComponent from "./GridStyles"
 
-function Grid({ filter, items }) {
+import Button from "../../atoms/button/Button"
+
+type GridProps = {
+  filter?: boolean
+  items?: []
+  rows: [
+    {
+      altText: string
+      ext: string
+      filter?: boolean
+      key: string
+      mediaItemUrl: string
+      md?: string
+      tag: string
+      title: string
+    }
+  ]
+}
+
+const Grid = ({ filter, items, rows = [] }: GridProps) => {
   const [count, updateCount] = useState(100)
+
+  if (!items) return null
 
   const addItems = () => {
     updateCount(count + 9)
   }
 
-  var rows = []
   for (var i = 0; i < count; i++) {
     const item = items[i]
-
     if (typeof item === "undefined") break
 
-    const { id, media } = item
-    const { altText, md, mediaItemUrl } = media
+    const {
+      id,
+      media: { altText, md, mediaItemUrl },
+      tags,
+      title,
+    } = item
+
     const ext = mediaItemUrl.substr(mediaItemUrl.lastIndexOf(".") + 1)
 
-    if (ext === `mp4`) {
-      rows.push(
-        <GridItem
-          key={`grid-item-${id}`}
-          tag={item.tags.nodes[0].slug}
-          filter={filter}
-        >
-          <video src={mediaItemUrl} alt={altText} autoPlay muted loop />
-          <span className="grid__item__title">{item.title}</span>
-        </GridItem>
-      )
-    } else {
-      rows.push(
-        <GridItem
-          key={`grid-item-${id}`}
-          tag={item.tags.nodes[0].slug}
-          filter={filter}
-        >
-          <img src={md} alt={altText} />
-          <span className="grid__item__title">{item.title}</span>
-        </GridItem>
-      )
-    }
+    rows.push({
+      altText,
+      ext,
+      filter,
+      key: `grid-item-${id}`,
+      md,
+      mediaItemUrl,
+      tag: tags.nodes[0].slug,
+      title,
+    })
   }
 
   return (
     <>
-      <GridComponent>{rows}</GridComponent>
-      <button disabled={count > items.length} onClick={addItems}>
+      <GridComponent>
+        {rows.map(
+          ({ altText, ext, filter, key, mediaItemUrl, md, tag, title }) => (
+            <GridItem
+              altText={altText}
+              ext={ext}
+              filter={filter}
+              key={key}
+              md={md}
+              mediaItemUrl={mediaItemUrl}
+              tag={tag}
+              title={title}
+            />
+          )
+        )}
+      </GridComponent>
+      <Button disabled={count > items.length} onClick={addItems}>
         Load More
-      </button>
+      </Button>
     </>
   )
 }
 
-function GridItem({ tag, children, filter }) {
+function GridItem({ altText, ext, filter, key, mediaItemUrl, md, tag, title }) {
   const [fullScreen, changeFullScreen] = useState(false)
 
   const toggleFullscreen = e => {
@@ -70,8 +96,13 @@ function GridItem({ tag, children, filter }) {
   }
 
   return (
-    <div className={classList} onClick={toggleFullscreen}>
-      {children}
+    <div className={classList} key={key} onClick={toggleFullscreen}>
+      {ext === `mp4` ? (
+        <video src={mediaItemUrl} autoPlay={true} loop={true} muted={true} />
+      ) : (
+        <img alt={altText} src={md} />
+      )}
+      <span className="grid__item__title">{title}</span>
     </div>
   )
 }
