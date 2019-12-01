73d739d4c32c671585b13df1504e6b062f074b07
Jack Pritchard <jack@noface.co.uk>
2019-12-01 13:53:41 +0000

    Gatsby-Image - Presentations

--- src/components/organisms/presentations/Presentations.tsx
+++ src/components/organisms/presentations/Presentations.tsx
@@ -1,9 +1,11 @@
 import React from "react"
+import Img from "gatsby-image/withIEPolyfill"
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import ParseHTML from "../../particles/ParseHTML"
 import useQueryPresentations from "../../particles/hooks/useQueryPresentations"
+import { isFluid } from "../../helpers"
 
 import PresentationsComponent from "./PresentationsStyles"
 
@@ -18,6 +20,17 @@ type PresentationsProps = {
 type PresentationProps = {
   date: string
   featuredImage: {
+    imageFile?: {
+      childImageSharp?: {
+        fluid?: {
+          aspectRatio: number
+          base64: string
+          sizes: string
+          src: string
+          srcSet: string
+        }
+      }
+    }
     altText: string
     xs: string
     sm: string
@@ -35,6 +48,8 @@ type PresentationProps = {
 
 const Presentations = ({ content }: PresentationsProps) => {
   const presentations = useQueryPresentations()
+
+  console.log(presentations)
 
   return (
     <PresentationsComponent>
@@ -55,11 +70,25 @@ const Presentations = ({ content }: PresentationsProps) => {
 
 const Presentation = ({ featuredImage, title, venue }: PresentationProps) => (
   <div className="presentations__event" key={`${title}-${venue}`}>
-    <img
-      alt={title}
-      className="presentations__event__thumbnail"
-      src={featuredImage.sm}
-    />
+    {isFluid(featuredImage) ? (
+      <div>
+        <Img
+          alt={title}
+          className="presentations__event__thumbnail"
+          fluid={featuredImage.imageFile.childImageSharp.fluid}
+          imgStyle={{
+            objectFit: "cover",
+          }}
+          objectFit="cover"
+        />
+      </div>
+    ) : (
+      <img
+        alt={title}
+        className="presentations__event__thumbnail"
+        src={featuredImage.sm}
+      />
+    )}
     <div className="presentations__event__meta">
       <h5 className="subheading">{venue}</h5>
       <h3>{title}</h3>
--- src/components/organisms/presentations/PresentationsStyles.tsx
+++ src/components/organisms/presentations/PresentationsStyles.tsx
@@ -1,119 +1,123 @@
-import styled from "styled-components";
-import { device } from "../../particles/MediaQueries";
+import styled from "styled-components"
+import { device } from "../../particles/MediaQueries"
 
 const PresentationsComponent = styled.section`
-	margin: 32px 0;
+  margin: 32px 0;
 
-	@media ${device.xs} {
-		margin: 64px 0;
-	}
+  @media ${device.xs} {
+    margin: 64px 0;
+  }
 
-	@media ${device.lg} {
-		margin: 96px 0;
-	}
+  @media ${device.lg} {
+    margin: 96px 0;
+  }
 
-	a {
-		color: inherit;
-	}
+  a {
+    color: inherit;
+  }
 
-	h3 {
-		margin-top: 0;
+  h3 {
+    margin-top: 0;
 
-		font-weight: bold;
-		font-size: 24px;
-	}
+    font-weight: bold;
+    font-size: 24px;
+  }
 
-	img {
-		display: block;
-		max-height: 175px;
-		width: 100%;
+  img {
+    display: block;
+    max-height: 175px;
+    width: 100%;
 
-		object-fit: cover;
-	}
+    object-fit: cover;
+  }
 
-	.presentations__events {
-		align-items: flex-start;
-		display: flex;
-		flex: 1;
-		height: 100%;
-		position: relative;
-		left: 0;
-		top: 0;
+  .presentations__events {
+    align-items: flex-start;
+    display: flex;
+    flex: 1;
+    height: 100%;
+    position: relative;
+    left: 0;
+    top: 0;
 
-		a {
-			display: block;
-			text-decoration: none;
-		}
+    a {
+      display: block;
+      text-decoration: none;
+    }
 
-		p {
-			margin: 0;
-		}
-	}
+    p {
+      margin: 0;
+    }
+  }
 
-	.presentations__event__meta {
-		padding: 16px;
-	}
+  .presentations__event__meta {
+    padding: 16px;
+  }
 
-	.presentations__event__thumbnail {
-		border-radius: 3px 3px 0 0;
-		position: relative;
-	}
+  .presentations__event__thumbnail {
+    border-radius: 3px 3px 0 0;
+    position: relative;
+  }
 
-	.presentations__event__logo {
-		height: 32px;
-		position: absolute;
-		right: -16px;
-		top: -16px;
-		width: 32px;
+  .presentations__event__logo {
+    height: 32px;
+    position: absolute;
+    right: -16px;
+    top: -16px;
+    width: 32px;
 
-		background: #ea4c89;
-		border-radius: 50%;
-		fill: #c32361;
-	}
+    background: #ea4c89;
+    border-radius: 50%;
+    fill: #c32361;
+  }
 
-	.presentations__event {
-		border-radius: 0 0 3px 3px;
+  .presentations__event {
+    border-radius: 0 0 3px 3px;
 
-		background: ${props => props.white};
-		box-shadow: 0px 2px 6px rgba(20, 18, 19, 0.1);
-		transition: 0.2s all ease;
+    background: ${props => props.white};
+    box-shadow: 0px 2px 6px rgba(20, 18, 19, 0.1);
+    transition: 0.2s all ease;
 
-		&:active,
-		&:focus,
-		&:hover {
-			box-shadow: 0px 2px 10px rgba(20, 18, 19, 0.3);
-		}
-	}
+    &:active,
+    &:focus,
+    &:hover {
+      box-shadow: 0px 2px 10px rgba(20, 18, 19, 0.3);
+    }
+  }
 
-	.subheading {
-		margin: 0 0 8px;
+  .presentations__event__thumbnail > div {
+    padding-bottom: 56.25% !important; /* your aspect ratio */
+  }
 
-		color: ${props => props.theme.primary};
-		font-size: 10px;
-		font-weight: 500;
-		letter-spacing: 0.1em;
-		line-height: 1.2;
-		text-transform: uppercase;
-	}
+  .subheading {
+    margin: 0 0 8px;
 
-	.slick-slider {
-		padding: 0 0 8px;
-		width: 100%;
+    color: ${props => props.theme.primary};
+    font-size: 10px;
+    font-weight: 500;
+    letter-spacing: 0.1em;
+    line-height: 1.2;
+    text-transform: uppercase;
+  }
 
-		cursor: grab;
-	}
+  .slick-slider {
+    padding: 0 0 8px;
+    width: 100%;
 
-	.slick-list {
-		margin: 0 -16px;
-	}
+    cursor: grab;
+  }
 
-	.slick-slide {
-		padding: 0 16px;
+  .slick-list {
+    margin: 0 -16px;
+  }
 
-		@media ${device.md} {
-			padding: 16px;
-		}
-	}
-`;
+  .slick-slide {
+    padding: 0 16px;
 
-export default PresentationsComponent;
+    @media ${device.md} {
+      padding: 16px;
+    }
+  }
+`
+
+export default PresentationsComponent
